3e25c72143d3bf6b11277ffdf6b5e2af121f5b5f
Kleber Tavares <krebaum_baum@hotmail.com>
2014-05-10 01:36:00 -0400

    Add contact page, change layout, solve routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,25 +5,25 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+      visit '/'
+      expect(page).to have_content('Rola Blog')
     end
 
     it "should have the title 'home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      visit '/'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App ")
     end
   end
 
   describe "Help page" do
 
     it "should have the content 'Help'" do
-     visit '/static_pages/help'
+     visit '/help'
      expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit '/help'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
@@ -31,13 +31,26 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit '/about'
       expect(page).to have_content('About Us')
     end
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit '/about'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the content 'Contact'" do
+      visit '/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 end
